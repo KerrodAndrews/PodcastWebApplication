653e6eede538c4115d64f684d6ae7f4a45c1470b
KerrodAndrews <162085387+KerrodAndrews@users.noreply.github.com>
2025-02-25 14:02:05 +1300

    Create __init__.py

---
+++ podcast/blueprints/__init__.py
@@ -0,0 +1,8 @@
+"""from flask import Flask, render_template, request
+# Build the application - these steps require an application context.
+app = Flask(__name__)
+
+with app.app_context():
+    # Register blueprints.
+    from .blueprints import blueprints
+    app.register_blueprint(blueprints.home_blueprint)"""
